21c3963651de75d73309c1a8c334c6b07c522fae
oscar.melerocamacho <oscar.melerocamacho@plexus.es>
2024-10-07 17:06:05 +0200

    Json responses exercise

--- src/main/java/net/sfkao/activityPlanner/controller/HolaMundoController.java
+++ src/main/java/net/sfkao/activityPlanner/controller/HolaMundoController.java
@@ -1,14 +1,36 @@
 package net.sfkao.activityPlanner.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class HolaMundoController {
 
     @GetMapping("/holaMundo")
-    public String holaMundo(){
+    public String holaMundo() {
         return "Saludaciones";
+    }
+
+    @GetMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getJSON() {
+        List<String> list = Arrays.asList("1", "2", "3");
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/jsonretorno", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getJSONRetorno() {
+        Map<String, String> response = new HashMap<>();
+        response.put("saludo", "saludos");
+        response.put("despedida", "adiosito");
+        return ResponseEntity.of(Optional.of(response));
     }
 
 }
